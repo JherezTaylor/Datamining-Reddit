578eda34d29d16b99a56e1d7c9383272294d7d0c
JherezTaylor <jherez.taylor@gmail.com>
2015-12-25 00:00:02 +0800

    updated sqltojson. Accepts the subreddit as an argument and returns a json dump

--- redditComm/sqltojson.py
+++ redditComm/sqltojson.py
@@ -1,27 +1,34 @@
+"""This module accepts the name of a subreddit as an argument
+and selects and dumps all the records in the subreddit as a json object.
+Run it with python sqltojson.py <subreddit> (without brackets)"""
+
 #!/bin/python
- 
 import sqlite3
 import json
- 
+from sys import argv
+
+script, subreddit = argv
+
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
- 
+
 connection = sqlite3.connect("database.sqlite")
 connection.row_factory = dict_factory
- 
+
 cursor = connection.cursor()
- 
-cursor.execute("select * from May2015 LIMIT 10")
- 
+
+SQL = 'SELECT * FROM  May2015 WHERE subreddit = %s'%("'{}'".format(subreddit))
+
+cursor.execute(SQL)
 # fetch all or one we'll go for all.
- 
+
 results = cursor.fetchall()
 
-with open('database_dump.json', 'w+') as f:
+with open(subreddit+'_dump.json', 'w+') as f:
 	json.dump(results,f)
 f.closed
- 
+
 connection.close()
