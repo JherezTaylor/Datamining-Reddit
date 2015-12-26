4d5c5042c4132d693fb6a69fdfdabd095cebad7c
JherezTaylor <jherez.taylor@gmail.com>
2015-12-26 16:58:46 +0800

    testing file pickle

--- redditComm/sqltojson.py
+++ redditComm/sqltojson.py
@@ -3,11 +3,16 @@ and selects and dumps all the records in the subreddit as a json object.
 Run it with python sqltojson.py <subreddit> (without brackets)"""
 
 #!/bin/python
-import sqlite3
-import json
-from sys import argv
+import sqlite3, json, logging, os
+import cPickle as pickle
+from time import time
+# from sys import argv
 
-script, subreddit = argv
+# script, subreddit = argv
+
+logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.getLogger('requests').setLevel(logging.CRITICAL)
+logger = logging.getLogger(__name__)
 
 def dict_factory(cursor, row):
     d = {}
@@ -15,20 +20,30 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-connection = sqlite3.connect("database.sqlite")
-connection.row_factory = dict_factory
+def main():
+    connection = sqlite3.connect("subreddit_dumps/database.sqlite")
+    connection.row_factory = dict_factory
+    cursor = connection.cursor()
 
-cursor = connection.cursor()
+    subreddit_list = ['nba','hockey','nfl','soccer','baseball']
+    for sub in subreddit_list:
+        ts = time()
 
-SQL = 'SELECT * FROM May2015 WHERE subreddit = %s'%("'{}'".format(subreddit))
+        print "Executing query "+sub
+        SQL = 'SELECT * FROM May2015 WHERE subreddit = %s'%("'{}'".format(sub))
+        cursor.execute(SQL)
+        results = cursor.fetchall()
 
-cursor.execute(SQL)
-# fetch all or one we'll go for all.
+        f = open('subreddit_dumps/'+sub+'_dump.pkl', 'wb')
+        pickle.dump(results, f)
+        f.close()
+        print('Query for 'sub+' took {}s'.format(time() - ts))
 
-results = cursor.fetchall()
+        # with open(sub+'_dump.p', 'w+') as f:
+    	#        json.dump(results,f)
+        # f.closed
 
-with open(subreddit+'_dump.json', 'w+') as f:
-	json.dump(results,f)
-f.closed
+    connection.close()
 
-connection.close()
+if __name__ == '__main__':
+   main()
