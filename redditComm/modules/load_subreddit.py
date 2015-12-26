f8d1dfd2c08a34f1a4572be569f12bfcb63c30c7
JherezTaylor <jherez.taylor@gmail.com>
2015-12-26 17:32:49 +0800

    we pickling out here

--- redditComm/modules/load_subreddit.py
+++ redditComm/modules/load_subreddit.py
@@ -1,9 +1,9 @@
 """This module accepts the name of a prefetched subreddit json dump
 and loads and returns its contents as an object."""
-import subreddit_dumps, json
+import subreddit_dumps
+import cPickle as pickle
 
 def load(subreddit):
-    with open('./subreddit_dumps/'+subreddit+'_dump.json', 'r') as f:
-        data = json.load(f)
-    f.closed
+    f = open('subreddit_dumps/'+subreddit+'_dump.pkl', 'r')
+    data = pickle.load(f)
     return data
