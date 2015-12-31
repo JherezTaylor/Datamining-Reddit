e9511cb55fdeb7f3b7fda896d7eaf86816d7afef
Alexander Barahona <Alexander Barahona>
2015-12-31 01:23:07 -0600

    Tried to build community but too many nodes, curse of dimensionality

---
+++ baseball_graph.R
@@ -0,0 +1,104 @@
+library(dplyr)
+library(reshape2)
+library(igraph)
+
+data <- read.csv(file="data.csv",head=TRUE,sep=",")
+
+#STEP 1: FINDING THE TEAMS AND THE NUMBER OF AUTHORS WITHIN EACH TEAM. 
+authors.in.team <- group_by(data, author_flair_text)
+authors.in.team.sum <- as.data.frame(summarise(authors.in.team, counting = n_distinct(author)))
+
+    #plotting the data for different teams
+    names <- authors.in.team.sum$author_flair_text
+    barplot(authors.in.team.sum$counting,
+            legend=rownames(authors.in.team.sum$author_flair_text),
+            beside="TRUE", names.arg=names,  col = "sky blue", cex.names=0.6, las=2)
+
+#Finding the teams for the authors in our data
+teams <- distinct(select(data, author, author_flair_text))
+teams.df <- tbl_df(teams) #this makes a data frame matching authors with their team
+
+summarise(teams.df, n_distinct(author_flair_text), n_distinct(author))
+#Originally: Total number of teams: 103  , Total number of authors: 12500
+#After preprocessing: Total number of teams: 73, Total number of authors: 2411
+
+#STEP 2: CREATE THE ADJACENCY MATRIX FOR THE GRAPH
+authors.and.topics <- select(data, author, link_id) #with parent_id matrix is 5 million
+
+#convert it into an adjacent matrix so that we can plot the graph
+#http://web.stanford.edu/~messing/Affiliation%20Data.html
+M = as.matrix(table(authors.and.topics)) # restructure your network data in matrix format
+m = table(authors.and.topics)
+M = as.matrix(m)
+Mrow = M %*% t(M) #Mrow will be the one-mode matrix formed by the row entities. 110 MB
+
+#STEP 3: USING IGRAPH TO CONSTRUCT THE GRAPH
+#http://jfaganuk.github.io/2015/01/02/analyzing-a-basic-network/
+
+graph.data.order <- melt(Mrow, id.vars = c('author')) #Using graph.data.frame to reshape the matrix so that it is not wide, but tall
+colnames(graph.data.order) <- c('source','target','weight') # changing the column names to a different format
+graph.authors <- graph.data.frame(graph.data.order, directed = T) # make it into a graph data frame, the format for igraph
+graph.authors <- simplify(graph.authors, remove.loops = T, remove.multiple = F)#removing self loops
+
+#STEP 4: MATCHING THE TEAMS TO EACH OF THE NODES IN THE GRAPH
+#code source: http://www.shizukalab.com/toolkits/sna/plotting-networks-pt-2
+list.vertex.attributes(graph.authors) #this gets the attributes attached to the vertixes or nodes
+V(graph.authors)$team=as.character(teams.df$author_flair_text[match(V(graph.authors)$name,teams.df$author)]) #matching teams with authors
+
+#STEP 5: CREATING A SUBGRAPH WITH ELEMENTS THAT HAVE WEIGHT OVER 5 and PLOTTING
+# filter the network based on weight over 5... if we leave vertices with edges<5 then it looks very messy
+graph.authors.edge <- subgraph.edges(graph.authors, which(E(graph.authors)$weight >= 5))
+
+
+#Original network size
+vcount(graph.authors)
+ecount(graph.authors)
+# 2411 nodes / 5810510 edges
+
+#Subgraph
+vcount(graph.authors.edge)
+ecount(graph.authors.edge)
+#Weight 1: 2411 nodes / 442888 edges
+#Weight 2: 2315 nodes / 119558 edges
+#Weight 3: 1856 nodes / 47070 edges
+#Weight 4: 1468 nodes / 26480 edges
+#Weight 5: 1108 nodes / 15342 edges
+#Weight 6: 897 nodes / 11482 edges
+#Weight 7: 707 nodes / 8524 edges
+#Weight 8: 621 nodes / 7076 edges
+#Weight 9: 547 nodes / 5754 edges
+#Weight 10: 500 nodes / 5074 edges
+#Weight 20: 500 nodes / 5074 edges
+
+#First community algorithm: edge.betweenness.community
+com <- edge.betweenness.community(graph.authors.edge, modularity=TRUE, merges=TRUE)
+V(graph.authors.edge)$memb <- com$membership
+modularity(com)
+
+#Algorithm produces 691 communities... it is very sparse
+plot(com, graph.authors.edge, vertex.size=5, layout=layout.fruchterman.reingold, vertex.label=NA, edge.arrow.size=0.2, edge.color="dark grey", edge.label.font=0.5)
+
+#subgraph of the largest community
+sizes(com)
+subg <- induced.subgraph(graph.authors.edge, which(membership(com) >= x))
+teams.community <- tbl_df(as.data.frame(get.vertex.attribute(subg)))
+tkplot(subg)
+
+#STEP 10: Centrality and Power Measures
+#Degree
+degree(graph.authors.edge)
+degree(graph.authors.edge, mode = 'total')
+degree(graph.authors.edge, mode = 'in')
+degree(graph.authors.edge, mode = 'out')
+
+#Betweennes
+betweenness(graph.authors.edge)
+
+#closeness
+closeness(graph.authors.edge1)
+
+# extract the components
+g.components <- clusters(graph.authors.edge)
+
+# which is the largest component
+ix <- which.max(g.components$csize)
